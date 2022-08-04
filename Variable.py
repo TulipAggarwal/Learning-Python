bce731bfbf69804319d202a88dc3508159fd03d6
TulipAggarwal <aggarwaltulip@gmail.com>
2022-08-04 16:48:37 +0530

    Added file Variable.py

---
+++ Variable.py
@@ -0,0 +1,6 @@
+#Explaning the concepts of variables
+
+char_name = str(input(print("Enter characters name: ")))
+print ("There one was a man named" +char_name)
+char_age = str(input(print("Enter characters age: ")))
+print("He/She was " +char_age + " years old")
