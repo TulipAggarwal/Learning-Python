fe012848adfb4a13b367ed2d88c3af6970681827
TulipAggarwal <aggarwaltulip@gmail.com>
2022-08-04 16:10:26 +0530

    Added file variable.py

---
+++ Numbers.py
@@ -0,0 +1,17 @@
+#Working with numbers
+
+from math import *
+print(23.2) #Basic syntax to print a number
+print (2+3) #prints the sum of these numbers
+print (10%3) #prints the remainder of 10/3, called the module function
+num = -6 #storing value of 5 in a variable called num
+print(num) #printing the value of num variable
+print (str(num) + " My fav number") #converts the number to string type useful when we need to print number with strings
+print(abs(num)) #prints the absolute value of num
+print(pow(num,2)) #prints the value of num^2
+print(max(num,10)) #prints the number which is bigger amongst num and 10
+print(min(num,10)) #prints the number which is smaller amongst num and 10
+print(round(5.6)) #Rounds off number with basic rounding of frules
+print(floor(33.4)) #rounds a number down
+print(ceil(33.4)) #rounds a number up
+print(sqrt(54)) #prints the square root of a number
