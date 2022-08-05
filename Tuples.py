e019880f8588e8ddbcb6f8e221be33c42136471f
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-08-05 14:34:21 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,7 @@
+#Tuples in python
+
+coordinates = (2,3,5) #creating a tuple
+#Tuples are immutable which means they cannot be modified
+print(coordinates[2]) #prints the element at 2nd index in our tuple
+
+#We use tuples for storing data that is never going to change
