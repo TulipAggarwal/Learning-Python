a4d36aebc46c92b33e450977f5fadb16aa4c6aa9
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-08-05 12:31:56 +0530

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,9 @@
+#Python program for MadLibs game
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+anything = input ("Enter anything you love: ")
+
+print("Roses are " +color)
+print(plural_noun+ " are blue ")
+print ("We love " +anything)
