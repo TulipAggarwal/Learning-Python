4fe0b9bd5582c448dd5322379d89751da90133ff
TulipAggarwal <aggarwaltulip@gmail.com>
2022-08-05 12:07:50 +0530

    Added file InputFromUser.py

---
+++ InputFromUser.py
@@ -0,0 +1,5 @@
+#Taking inputs from the user
+name= str(input("Enter your name: ")) #Storing the input in a variable named name
+age = str(input("Enter your age: ")) #Storing the input in a variable named age
+print("Hello " + name + " !" + " ,your age is " + age)
+
