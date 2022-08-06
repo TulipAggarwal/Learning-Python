f5d14de713994db4649689d590c3cf8e342b7d8e
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-08-06 23:35:51 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,7 @@
+#Learning about while loop in python
+
+i = 1
+while i <=10: #our condition for while loop
+    print(i)
+    i+= 1 #Incrementing value of i by 1 in each iteration
+print ("Loop ended") #A print statement to print that the loop has ended
