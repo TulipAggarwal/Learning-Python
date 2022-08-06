e1cea8a598f45b578a2e4e12cf0db53578d6c01e
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-08-06 22:21:39 +0530

    Add files via upload

---
+++ Func.py
@@ -0,0 +1,6 @@
+#Python function for printing Hello
+#Always better to name functions in lowercase
+def sayhello(name, age): #definig the function with thwo parameters
+    print("Hello " + name +" you are " +age) #body of the function
+sayhello("Tulip", "20") #calling the function using a parameter
+sayhello("Tara", "18") #Another call for the same function
