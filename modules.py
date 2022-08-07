460d27d866b54774422729de879ead9c5e6af1f4
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-08-07 13:26:10 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,9 @@
+#modules & pip in python
+
+#importing a function from the toolsfile.py
+import toolsfile
+print(toolsfile.roll_dice(10))
+
+#for finding various other useful modules you can visit:
+# https://docs.python.org/3/py-modindex.html
+
