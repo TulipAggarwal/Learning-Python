b2022be16fd947f18b19ddf57148644cf14a6ac1
Tulip Aggarwal <93984886+TulipAggarwal@users.noreply.github.com>
2022-08-07 13:49:54 +0530

    Add files via upload

---
+++ classobj.py
@@ -0,0 +1,8 @@
+
+#creating an object using student.py class template
+
+from student import student
+student1 = student("Tara", "python", 9.0, False)
+print(student1.name)
+
+#we can create numerous such objects for student template class
